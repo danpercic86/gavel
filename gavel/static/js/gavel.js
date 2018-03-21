2be46cd51b0b569fcb4a1a1310ab718aad637c05
Adrian Meresescu <adrian.meresescu@gmail.com>
2018-03-22 00:26:01 +0200

    Initial changes to voting page

--- gavel/static/js/gavel.js
+++ gavel/static/js/gavel.js
@@ -1,3 +1,9 @@
-(function(){
-    
-}());
+$(document).ready(function(){
+
+    // manage team selection and unlock vote button
+    $('.label-team').click(function(){
+        $('.label-team').removeClass('active');
+        $(this).addClass('active');
+        $('.button-vote').show();
+    });
+});
