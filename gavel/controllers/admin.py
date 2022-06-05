563b7f71eaf4f95cbe56b2b90ef4f799b6fe9ada
Dan Percic <danpercic86@gmail.com>
2022-06-05 18:30:14 +0300

    update judge actions

--- gavel/controllers/admin.py
+++ gavel/controllers/admin.py
@@ -1,24 +1,15 @@
-from typing import List
-
-import psycopg2.errors
 from flask import (
     redirect,
     request,
     url_for,
 )
-from sqlalchemy.exc import IntegrityError
 
-import gavel.settings as settings
 import gavel.utils as utils
 from gavel import app
-from gavel.controllers.admins.judges import judge_login_link
-from gavel.controllers.admins.projects import parse_upload_form
 from gavel.models import (
-    Annotator,
     Item,
     db,
     with_retries,
-    ignore_table,
 )
 
 
@@ -46,88 +37,3 @@ def item_patch():
     with_retries(tx)
 
     return redirect(url_for("item_detail", item_id=item.id))
-
-
-@app.route("/admin/annotator", methods=["POST"])
-@utils.requires_auth
-def annotator_actions():
-    action = request.form["action"]
-
-    if action == "Submit":
-        data = parse_upload_form()
-        added = []
-        if data:
-            # validate data
-            for index, row in enumerate(data):
-                if len(row) != 3:
-                    return utils.user_error(
-                        "Bad data: row %d has %d elements (expecting 3)"
-                        % (index + 1, len(row))
-                    )
-
-            def tx():
-                for row in data:
-                    annotator = Annotator(*row)
-                    added.append(annotator)
-                    db.session.add(annotator)
-                db.session.commit()
-
-            with_retries(tx)
-            try:
-                email_invite_links(added)
-            except Exception as e:
-                return utils.server_error(str(e))
-    elif action == "Email":
-        annotator_id = request.form["annotator_id"]
-        try:
-            email_invite_links(Annotator.by_id(annotator_id))
-        except Exception as e:
-            return utils.server_error(str(e))
-    elif action == "Disable" or action == "Enable":
-        annotator_id = request.form["annotator_id"]
-        target_state = action == "Enable"
-
-        def tx():
-            Annotator.by_id(annotator_id).active = target_state
-            db.session.commit()
-
-        with_retries(tx)
-    elif action == "Delete":
-        annotator_id = request.form["annotator_id"]
-        try:
-
-            def tx():
-                db.session.execute(
-                    ignore_table.delete(ignore_table.c.annotator_id == annotator_id)
-                )
-                Annotator.query.filter_by(id=annotator_id).delete()
-                db.session.commit()
-
-            with_retries(tx)
-        except IntegrityError as e:
-            if isinstance(e.orig, psycopg2.errors.ForeignKeyViolation):
-                return utils.server_error(
-                    "Judges can't be deleted once they have voted on a project. You can use the 'disable' functionality instead, which has a similar effect, locking out the judge and preventing them from voting on any other projects."
-                )
-            else:
-                return utils.server_error(str(e))
-    return redirect(url_for("admin"))
-
-
-def email_invite_links(annotators: List[Annotator] | Annotator):
-    if settings.DISABLE_EMAIL or annotators is None:
-        return
-    if not isinstance(annotators, list):
-        annotators = [annotators]
-
-    emails = []
-    for annotator in annotators:
-        link = judge_login_link(annotator)
-        raw_body = settings.EMAIL_BODY.format(name=annotator.name, link=link)
-        body = "\n\n".join(utils.get_paragraphs(raw_body))
-        emails.append((annotator.email, settings.EMAIL_SUBJECT, body))
-
-    if settings.USE_SENDGRID and settings.SENDGRID_API_KEY is not None:
-        utils.send_sendgrid_emails(emails)
-    else:
-        utils.send_emails.delay(emails)
--- gavel/controllers/admins/judges.py
+++ gavel/controllers/admins/judges.py
@@ -1,18 +1,34 @@
+from typing import List
+
+import psycopg2.errors
 from flask import (
-    render_template,
+    redirect,
+    request,
     url_for,
 )
+from flask import (
+    render_template,
+)
+from sqlalchemy.exc import IntegrityError
 
+import gavel.settings as settings
 import gavel.stats as stats
 import gavel.utils as utils
 from gavel import app
+from gavel.controllers.admins.projects import parse_upload_form
 from gavel.models import (
     Annotator,
-    Decision,
     Item,
+    db,
+    with_retries,
+    ignore_table,
+)
+from gavel.models import (
+    Decision,
 )
 
 ALLOWED_EXTENSIONS = {"csv", "xlsx", "xls"}
+FOREIGN_KEY_ERROR = "Judges can't be deleted once they have voted on a project. You can use the 'disable' functionality instead, which has a similar effect, locking out the judge and preventing them from voting on any other projects."
 
 
 @app.route("/admin/judges")
@@ -62,6 +78,112 @@ def judge_details(judge_id: str):
         seen=seen,
         skipped=skipped,
     )
+
+
+@app.route("/admin/judges", methods=["POST"])
+@utils.requires_auth
+def upload_judge():
+    data = parse_upload_form()
+
+    if not data:
+        return utils.user_error("No judges uploaded")
+
+    added = []
+
+    for index, row in enumerate(data):
+        if len(row) != 3:
+            return utils.user_error(
+                f"Bad data: row {index + 1:d} has {len(row):d} elements (expecting 3)"
+            )
+
+    def tx():
+        for row in data:
+            annotator = Annotator(*row)
+            added.append(annotator)
+            db.session.add(annotator)
+        db.session.commit()
+
+    with_retries(tx)
+
+    if not settings.DEBUG:
+        try_send_emails(added)
+    else:
+        print("DEBUG: Not sending emails")
+
+    return redirect(url_for("admin_judges"))
+
+
+@app.route("/admin/judges/<judge_id>/delete", methods=["POST"])
+@utils.requires_auth
+def delete_judge(judge_id: str):
+    try:
+
+        def tx():
+            db.session.execute(
+                ignore_table.delete(ignore_table.c.annotator_id == judge_id)
+            )
+            Annotator.query.filter_by(id=judge_id).delete()
+            db.session.commit()
+
+        with_retries(tx)
+    except IntegrityError as e:
+        if isinstance(e.orig, psycopg2.errors.ForeignKeyViolation):
+            return utils.server_error(FOREIGN_KEY_ERROR)
+        else:
+            return utils.server_error(str(e))
+
+    return redirect(url_for("admin_judges"))
+
+
+@app.route("/admin/judges/<judge_id>/email", methods=["POST"])
+@utils.requires_auth
+def email_judge(judge_id: str):
+    try:
+        email_invite_links(Annotator.by_id(judge_id))
+    except Exception as e:
+        return utils.server_error(str(e))
+
+    return redirect(url_for("admin_judges"))
+
+
+@app.route("/admin/judges/<judge_id>/update-status", methods=["POST"])
+@utils.requires_auth
+def update_judge_status(judge_id: str):
+    new_state = request.form["value"] == "Enable"
+
+    def tx():
+        Annotator.by_id(judge_id).active = new_state
+        db.session.commit()
+
+    with_retries(tx)
+
+    return redirect(url_for("admin_judges"))
+
+
+def try_send_emails(added: List[Annotator]):
+    try:
+        email_invite_links(added)
+    except Exception as e:
+        return utils.server_error(str(e))
+
+
+def email_invite_links(annotators: List[Annotator] | Annotator):
+    if settings.DISABLE_EMAIL or annotators is None:
+        return
+    if not isinstance(annotators, list):
+        annotators = [annotators]
+
+    emails = []
+    for annotator in annotators:
+        link = judge_login_link(annotator)
+        raw_body = settings.EMAIL_BODY.format(name=annotator.name, link=link)
+        body = "\n\n".join(utils.get_paragraphs(raw_body))
+        emails.append((annotator.email, settings.EMAIL_SUBJECT, body))
+
+    if settings.USE_SENDGRID and settings.SENDGRID_API_KEY is not None:
+        utils.send_sendgrid_emails(emails)
+    else:
+        utils.send_emails.delay(emails)
 
 
 def judge_login_link(judge: Annotator):
