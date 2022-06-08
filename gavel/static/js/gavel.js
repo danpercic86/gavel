943ac7cf8e7d4af57a73f0751850ca255df3f319
Dan Percic <danpercic86@gmail.com>
2022-06-08 18:01:59 +0300

    fix voting alerts

--- gavel/static/js/gavel.js
+++ gavel/static/js/gavel.js
@@ -1,117 +1,79 @@
+function updateTimer(start, container) {
+    const startDateTime = new Date(start);
+    const startStamp = startDateTime.getTime();
+
+    function updateClock() {
+        const newStamp = new Date().getTime();
+        const diff = Math.round((newStamp - startStamp) / 1000);
+        const {m, s} = getTime(diff);
+
+        if (m >= timePerProject) {
+            $(container).parent('div').addClass('alert-blink');
+
+            if (m >= maxTimePerProject) {
+                $('body').addClass('alert-blink');
+            }
+        }
+
+        $(container).html(m + " m : " + s + " s");
+    }
+
+    setInterval(updateClock, 1000);
+}
+
+function getTime(diff) {
+    const d = Math.floor(diff / (24 * 60 * 60));
+    diff = diff - (d * 24 * 60 * 60);
+    const h = Math.floor(diff / (60 * 60));
+    diff = diff - (h * 60 * 60);
+    const m = Math.floor(diff / (60));
+    const s = diff % 60;
+    return {h, m, s};
+}
+
+function calculateTimeLeft() {
+    const newStamp = new Date().getTime();
+
+    let diff = Math.round((endJuryTime - newStamp) / 1000);
+    const {h, m} = getTime(diff);
+
+    $('#time-left').html(h + " h : " + m + " m");
+}
+
+function setCookie(key, value) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(set, key) {
+    const keyValue = document.cookie.match('(^|;) ?' + set + '=([^;]*)(;|$)');
+    const rawValue = keyValue ? keyValue[2] : null;
+    return (JSON.parse(rawValue) || {})[key];
+}
+
+
 $(document).ready(function () {
+    // manage team selection and unlock vote button
+    $('.label-team').click(function () {
+        $('.label-team').removeClass('active');
+        $(this).addClass('active');
+        $('.button-vote').show();
 
+        window.scrollTo(0, document.body.scrollHeight);
 
-    // implement tabs
-    $('#myTab a').on('click', function (e) {
-        e.preventDefault()
-        $(this).tab('show');
     });
 
-
-    console.log('hello gavel')
-    if ($('body').hasClass('judge-view')) {
-
-    console.log('i am judge')
-
-        // manage team selection and unlock vote button
-        $('.label-team').click(function () {
-            $('.label-team').removeClass('active');
-            $(this).addClass('active');
-            $('.button-vote').show();
-
-            window.scrollTo(0, document.body.scrollHeight);
-
-        });
-
-        if (currentProject) {
-            var date = new Date();
-            var tempData = {
-                'id': currentProject,
-                'timeStart': date.getTime()
-            };
-            setCookie('projectCounter', JSON.stringify(tempData));
-            $('.overlay').hide();
-
-            updateTimer(getCookie('projectCounter', 'timeStart'), '#time-spent');
-        }
-
-        if (getCookie('projectCounter', 'id') == currentProject) {
-            $('.overlay').hide();
-            updateTimer(getCookie('projectCounter', 'timeStart'), '#time-spent');
-        }
-
-
-        function setCookie(key, value) {
-            var expires = new Date();
-            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-        }
-
-        function getCookie(set, key) {
-            var keyValue = document.cookie.match('(^|;) ?' + set + '=([^;]*)(;|$)');
-            var rawValue = keyValue ? keyValue[2] : null;
-            return (JSON.parse(rawValue) || {})[key];
-        }
-
-        function updateTimer(start, container) {
-            var startDateTime = new Date(start);
-            var startStamp = startDateTime.getTime();
-
-            var newDate = new Date();
-            var newStamp = newDate.getTime();
-            var timer;
-
-            function updateClock() {
-                newDate = new Date();
-                newStamp = newDate.getTime();
-                var diff = Math.round((newStamp - startStamp) / 1000);
-
-                var d = Math.floor(diff / (24 * 60 * 60));
-                diff = diff - (d * 24 * 60 * 60);
-                var h = Math.floor(diff / (60 * 60));
-                diff = diff - (h * 60 * 60);
-                var m = Math.floor(diff / (60));
-                diff = diff - (m * 60);
-                var s = diff;
-
-                if (m >= timePerProject) {
-                    $(container).parent('div').addClass('alert-blink');
-
-                    if (m >= maxTimePerProject) {
-                        $('body').addClass('alert-blink');
-                    }
-                }
-
-                $(container).html(m + " m : " + s + " s");
-            }
-
-            setInterval(updateClock, 1000);
-        }
-
-        function updateTimeLeft() {
-            console.log('eeeeee')
-            var newDate = new Date();
-            var newStamp = newDate.getTime();
-
-            function calculateTimeLeft() {
-                newDate = new Date();
-                newStamp = newDate.getTime();
-
-                var diff = Math.round((endJuryTime - newStamp) / 1000);
-                var d = Math.floor(diff / (24 * 60 * 60));
-                diff = diff - (d * 24 * 60 * 60);
-                var h = Math.floor(diff / (60 * 60));
-                diff = diff - (h * 60 * 60);
-                var m = Math.floor(diff / (60));
-                diff = diff - (m * 60);
-                var s = diff;
-
-                $('#time-left').html(h + " h : " + m + " m");
-            }
-
-            setInterval(calculateTimeLeft, 1000);
-        }
-
-        updateTimeLeft();
+    if (getCookie('projectCounter', 'id') === currentProject) {
+        $('.overlay').hide();
+        updateTimer(getCookie('projectCounter', 'timeStart'), '#time-spent');
+    } else {
+        const tempData = {'id': currentProject, 'timeStart': new Date().getTime()};
+        setCookie('projectCounter', JSON.stringify(tempData));
+        $('.overlay').hide();
+        updateTimer(getCookie('projectCounter', 'timeStart'), '#time-spent');
     }
+
+
+    setInterval(calculateTimeLeft, 1000);
 });
